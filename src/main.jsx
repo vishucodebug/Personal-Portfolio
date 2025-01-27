afb5fc857214489d9754db74ad03ba5fb2898dbe
Vishal maurya <vishu.codebug@gmail.com>
2025-01-27 11:22:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
       <Route index element={<App />} />
-      <Route path="Gallary" element={<Gallary />} />
+      <Route path="/Gallary" element={<Gallary />} />
     </Routes>
   </BrowserRouter>
 );
